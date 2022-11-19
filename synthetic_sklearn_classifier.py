d6b966f8f21ff875724a513c6757a9bc42c9967e
Petro <kroshnyak_py@fizmat.tnpu.edu.ua>
2022-11-19 11:24:41 +0200

    Виправив друге завдання

--- synthetic_sklearn_classifier.py
+++ synthetic_sklearn_classifier.py
@@ -4,7 +4,7 @@ from sklearn.datasets import make_blobs
 import matplotlib.pyplot as plt
 from sklearn.model_selection import train_test_split
 from matplotlib.colors import ListedColormap
-from sklearn.neighbors import KNeighborsRegressor
+from sklearn.neighbors import KNeighborsClassifier
 from sklearn.preprocessing import StandardScaler
 cmap_bold = ListedColormap(['blue','#FFFF00','black','green'])
 
@@ -27,19 +27,19 @@ best_score = 0
 n = 0
 
 for i in range(1, 226):
-    knn_reg = KNeighborsRegressor(n_neighbors=i).fit(X_train_scaled, y_train)
+    knn = KNeighborsClassifier(n_neighbors=i).fit(X_train_scaled, y_train)
     #print(i, knn_reg.score(X_test_scaled, y_test))
-    if knn_reg.score(X_test_scaled, y_test) > best_score:
-        best_score = knn_reg.score(X_test_scaled, y_test)
+    if knn.score(X_test_scaled, y_test) > best_score:
+        best_score = knn.score(X_test_scaled, y_test)
         n = i
 
 print ('The best k = {} , score = {}'.format(n, best_score))
 
-knn_reg = KNeighborsRegressor(n_neighbors=n).fit(X_train_scaled, y_train)
-score = knn_reg.score(X_test_scaled, y_test)
+knn = KNeighborsClassifier(n_neighbors=n).fit(X_train_scaled, y_train)
+score = knn.score(X_test_scaled, y_test)
 
-draw.boundary_plot(knn_reg, X_train_scaled, y_train, X_test=X_test_scaled, y_test=y_test,
-                   title='KNN regression K= {}, score = {:.2f}'.format(n, score))
+draw.boundary_plot(knn, X_train_scaled, y_train, X_test=X_test_scaled, y_test=y_test,
+                   title='KNN classification K= {}, score = {:.2f}'.format(n, score))
 
 
 #plt.figure()
